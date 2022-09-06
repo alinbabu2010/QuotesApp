df1bc48348f93e72e410b9e3436ef6052f50a0b4
Alin Babu <alinbabu2010@gmail.com>
2022-09-06 18:45:19 +0530

    Add paging adapter class for quotes

---
+++ app/src/main/java/com/sample/quotesapp/ui/QuotesAdapter.kt
@@ -0,0 +1,48 @@
+package com.sample.quotesapp.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sample.quotesapp.data.models.Quotes
+import com.sample.quotesapp.databinding.ItemQuotesBinding
+import javax.inject.Inject
+
+class QuotesAdapter @Inject constructor() :
+    PagingDataAdapter<Quotes, QuotesAdapter.QuotesViewHolder>(QuotesDiffUtils()) {
+
+    class QuotesViewHolder(val binding: ItemQuotesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Quotes) {
+            binding.quote.text = item.content
+        }
+
+    }
+
+    class QuotesDiffUtils : DiffUtil.ItemCallback<Quotes>() {
+
+        override fun areItemsTheSame(oldItem: Quotes, newItem: Quotes): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Quotes, newItem: Quotes): Boolean =
+            oldItem == newItem
+
+    }
+
+    override fun onBindViewHolder(holder: QuotesViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuotesViewHolder =
+        QuotesViewHolder(
+            ItemQuotesBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+
+}
