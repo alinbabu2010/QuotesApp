27a13150fef24796aed6c0aaf90daf814b6bf0f3
Alin Babu <alinbabu2010@gmail.com>
2022-09-06 18:43:26 +0530

    Add interface module for DI

---
+++ app/src/main/java/com/sample/quotesapp/di/InterfaceModule.kt
@@ -0,0 +1,27 @@
+package com.sample.quotesapp.di
+
+import com.sample.quotesapp.data.repositories.QuotesListRepository
+import com.sample.quotesapp.data.repositories.QuotesRepository
+import com.sample.quotesapp.data.sources.remote.QuotesInternetDataSource
+import com.sample.quotesapp.data.sources.remote.QuotesNetworkDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface InterfaceModule {
+
+    @Binds
+    fun bindQuotesNetworkDataSource(
+        quotesInternetDataSource: QuotesInternetDataSource
+    ): QuotesNetworkDataSource
+
+    @Binds
+    fun bindQuotesRepository(
+        quotesListRepository: QuotesListRepository
+    ): QuotesRepository
+
+
+}
