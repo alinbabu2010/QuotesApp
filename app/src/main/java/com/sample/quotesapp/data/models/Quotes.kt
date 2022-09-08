5c060df8142b04e95cdbb66233537b6afd4d51a8
Alin Babu <alinbabu2010@gmail.com>
2022-09-08 17:38:39 +0530

    Modify `Quotes` data class for database entity

--- app/src/main/java/com/sample/quotesapp/data/models/Quotes.kt
+++ app/src/main/java/com/sample/quotesapp/data/models/Quotes.kt
@@ -1,14 +1,17 @@
 package com.sample.quotesapp.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "Quotes")
 data class Quotes(
+    @PrimaryKey(autoGenerate = false)
     @SerializedName("_id") val id: String,
     val author: String,
     val authorSlug: String,
     val content: String,
     val dateAdded: String,
     val dateModified: String,
-    val length: Int,
-    val tags: List<String>
+    val length: Int
 )
