5876ad7001e03dce84defb55c7b0b3e79b125836
Alin Babu <alinbabu2010@gmail.com>
2022-09-08 12:24:13 +0530

    Add load state adapter and layout

---
+++ app/src/main/java/com/sample/quotesapp/ui/adapters/LoaderAdapter.kt
@@ -0,0 +1,32 @@
+package com.sample.quotesapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sample.quotesapp.databinding.LoaderItemBinding
+
+class LoaderAdapter : LoadStateAdapter<LoaderAdapter.LoadStateViewHolder>() {
+
+    class LoadStateViewHolder(val binding: LoaderItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(loadState: LoadState) {
+            binding.progressBar.isVisible = loadState is LoadState.Loading
+        }
+    }
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState) = LoadStateViewHolder(
+        LoaderItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
+    
+}
