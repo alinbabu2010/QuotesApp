0279fa06705c08938e5dd896e62f8377fe644021
Alin Babu <alinbabu2010@gmail.com>
2022-09-08 17:39:25 +0530

    Add data class for remote keys database entity

---
+++ app/src/main/java/com/sample/quotesapp/data/models/QuoteRemoteKeys.kt
@@ -0,0 +1,12 @@
+package com.sample.quotesapp.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class QuoteRemoteKeys(
+    @PrimaryKey(autoGenerate = false)
+    val id: String,
+    val prevPage: Int?,
+    val nextPage: Int?
+)
