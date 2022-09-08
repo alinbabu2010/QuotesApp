759a05f5f22a65c38eec6e123c0cf3fd55623fd2
Alin Babu <alinbabu2010@gmail.com>
2022-09-08 17:39:55 +0530

    Add DAO for quotes and remote keys

---
+++ app/src/main/java/com/sample/quotesapp/data/sources/local/QuotesDao.kt
@@ -0,0 +1,22 @@
+package com.sample.quotesapp.data.sources.local
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sample.quotesapp.data.models.Quotes
+
+@Dao
+interface QuotesDao {
+
+    @Query("SELECT * FROM Quotes")
+    fun getQuotes() : PagingSource<Int, Quotes>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addQuotes(quotes: List<Quotes>)
+
+    @Query("DELETE FROM Quotes")
+    suspend fun deleteQuotes()
+
+}
---
+++ app/src/main/java/com/sample/quotesapp/data/sources/local/RemoteKeysDao.kt
@@ -0,0 +1,21 @@
+package com.sample.quotesapp.data.sources.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sample.quotesapp.data.models.QuoteRemoteKeys
+
+@Dao
+interface RemoteKeysDao {
+
+    @Query("SELECT * FROM QuoteRemoteKeys WHERE id=:id")
+    suspend fun getRemoteKeys(id:String) : QuoteRemoteKeys
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAllRemoteKeys(keys: List<QuoteRemoteKeys>)
+
+    @Query("DELETE FROM QuoteRemoteKeys")
+    suspend fun deleteAllRemoteKeys()
+
+}
